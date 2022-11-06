ebbcc9fdae9c0faf5a429385d6e1fe79abd0b435
alagu-p <alagappan.plr@gmail.com>
2022-11-06 23:14:54 +0530

    Day-55 React js Folder Structure learning

--- src/App.js
+++ src/App.js
@@ -2,9 +2,11 @@ import logo from './logo.svg';
 import './App.css';
 
 const App=()=> {
+  const name1='Alagu';
+  const isNameShowing=true;
   return (
     <div className="App">
-     Hello world
+     <h1> Hello, {name1}!</h1>
     </div>
   );
 }
